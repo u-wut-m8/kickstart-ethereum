a18e722be80b87e16341d96301e999a0c4d7c068
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2018-12-31 12:17:59 +0530

    Finish test script.

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -34,4 +34,63 @@ describe("Campaigns", () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+
+  it("should mark campaign deployer as the manager", async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it("should allow people to contribute money and marks them as contributors", async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[1],
+      value: "200"
+    });
+    const isContributor = await campaign.methods.contributors(accounts[1]).call();
+    assert(isContributor);
+  });
+
+  it("should require a minimum contribution", async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: "5",
+        from: accounts[2]
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+
+  it("should allow a manager to make a payment request", async () => {
+    await campaign.methods.createRequest("buy batteries", "100", accounts[2]).send({from: accounts[0], gas: "1000000"});
+    const request = await campaign.methods.requests(0).call();
+    assert.equal(request.description, "buy batteries");
+  });
+
+  it('processes requests', async () => {
+   await campaign.methods.contribute().send({
+     from: accounts[0],
+     value: web3.utils.toWei('10', 'ether')
+   });
+
+   await campaign.methods
+     .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+     .send({from: accounts[0], gas: '1000000'});
+
+   await campaign.methods.approveRequest(0).send({
+     from: accounts[0],
+     gas: '1000000'
+   });
+
+   await campaign.methods.finalizeRequest(0).send({
+     from: accounts[0],
+     gas: '1000000'
+   });
+
+   let balance = await web3.eth.getBalance(accounts[1]);
+   balance = web3.utils.fromWei(balance, 'ether');
+   balance = parseFloat(balance);
+
+   assert(balance > 104);
+ });
 });
