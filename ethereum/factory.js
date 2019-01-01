8e2a527897034f563e0cb80b374093c59dbcf5a7
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2019-01-01 16:40:47 +0530

    Add next.js support.

---
+++ ethereum/factory.js
@@ -0,0 +1,9 @@
+import Factory from "./build/Factory.json";
+import web3 from "./web3";
+
+const instance = new web.eth.Contract(
+  JSON.parse(Factory.interface),
+  "0xe6A319EC182D04D4B98f29635D885BEC19997126"
+);
+
+export default instance;
